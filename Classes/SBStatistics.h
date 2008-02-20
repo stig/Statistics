8496f2d45737fdacd3a1893c448ccb8a77822914
Stig Brautaset <stig@brautaset.org>
2008-02-20 09:54:41 +0000

    re-arrange header and add some documentation links

--- Classes/SBStatistics.h
+++ Classes/SBStatistics.h
@@ -20,22 +20,34 @@
     double pseudoVariance;
 }
 
+// Methods for adding data points
+- (void)addData:(id)x;
+- (void)addDataFromArray:(NSArray*)x;
+
+// Count of data points
 @property(readonly) NSUInteger count;
+
+// Index of smallest and largest data points
 @property(readonly) NSUInteger mindex;
 @property(readonly) NSUInteger maxdex;
 
+// Value of smallest and largest data points
 @property(readonly) double min;
 @property(readonly) double max;
+
+// http://en.wikipedia.org/wiki/Arithmetic_mean
 @property(readonly) double mean;
 
+// http://en.wikipedia.org/wiki/Range_(statistics)
 - (double)range;
-- (double)variance;
-- (double)standardDeviation;
-- (double)biasedVariance;
-- (double)biasedStandardDeviation;
 
-- (void)addData:(id)x;
-- (void)addDataFromArray:(NSArray*)x;
+// http://en.wikipedia.org/wiki/Variance
+- (double)variance;                 // division by N-1
+- (double)biasedVariance;           // division by N
+
+// http://en.wikipedia.org/wiki/Standard_deviation
+- (double)standardDeviation;        // division by N-1
+- (double)biasedStandardDeviation;  // division by N
 
 @end
 
@@ -45,16 +57,27 @@
     NSArray *sortedData;
 }
 
+// http://en.wikipedia.org/wiki/Mode_(statistics)
 - (double)mode;
+
+// http://en.wikipedia.org/wiki/Median
 - (double)median;
-- (double)trimmedMeanWithPercentile:(double)x;
-- (double)trimmedMeanWithHighPercentile:(double)x low:(double)y;
+
+// http://en.wikipedia.org/wiki/Harmonic_mean
 - (double)harmonicMean;
+
+// http://en.wikipedia.org/wiki/Geometric_mean
 - (double)geometricMean;
 
+// http://en.wikipedia.org/wiki/Truncated_mean
+- (double)trimmedMeanWithPercentile:(double)x;
+- (double)trimmedMeanWithHighPercentile:(double)x low:(double)y;
+
+// http://en.wikipedia.org/wiki/Frequency_distribution
 - (NSDictionary*)frequencyDistributionWithPartitions:(NSUInteger)x;
 - (NSDictionary*)frequencyDistributionWithBuckets:(NSArray*)x;
 
+// returns the raw data points
 - (NSArray*)data;
 - (NSArray*)sortedData;
 
