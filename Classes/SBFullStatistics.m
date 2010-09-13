d837dd200aa341cf928a3c11d724ffaddb4d9a32
Stig Brautaset <stig@brautaset.org>
2010-09-13 03:55:12 +0100

    oops.

--- Classes/SBFullStatistics.m
+++ Classes/SBFullStatistics.m
@@ -28,8 +28,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #import "SBStatistics.h"
-#import "SBMutableDictionary.h"
-
 
 static void incrementValueForKey(NSMutableDictionary *dict, NSNumber *key)
 {
